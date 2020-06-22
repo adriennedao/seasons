f9634f45c3e0677654d22d6d4e1c4827b8b47778
Adrienne Dao <adriennedao@gmail.com>
2020-06-22 15:23:23 -0400

    Add condition for lat

--- src/index.js
+++ src/index.js
@@ -5,21 +5,36 @@ class App extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { lat: null };
-    }
-
-    // React says we have to define render !!
-    render() {
-        window.navigator.geolocation.getCurrentPosition(
-            position => console.log(position),
-            err => console.log(err)     
+     // This is the only time we do direct assignement
+     // to this.state 
+    this.state = { lat: null, errorMessage: '' };
+    
+     window.navigator.geolocation.getCurrentPosition(
+        position => {
+            // we called setstate!!!
+            this.setState({ lat: position.coords.lattitude });
+            },   
+            err => console.log(err)    
         );
-
-        return <div>Latitude: </div>; 
     }
-}
+   
+   
+    // React says we have to define render!!
+    render() {
+        if (this.state.errorMessage && !this.state.lat)  {
+            return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+        return <div>Latitude: {this.state.lat}</div>;
+    }
+    
+    return <div>Loading!</div>;
+    }
+}   
 
 ReactDOM.render(
     <App />,
     document.querySelector('#root'));
 
+  
