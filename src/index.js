ef012fdd571f090fd736409390d0bfe1023fc063
Adrienne Dao <adriennedao@gmail.com>
2020-10-06 11:51:46 -0400

    added sem ui icons

--- src/index.js
+++ src/index.js
@@ -3,33 +3,27 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-   
-    state = { lat: null, errorMessage: '' };
-     
-    compomentDidMount() {
-        window.navigator.geolocation.getCurrentPosition(
-        position =>  this.setState({ lat: position.coords.lattitude }), 
-            err => this.setState({ errorMessage: err.message })      
-        );
-    }
+  state = { lat: null, errorMessage: '' };
 
-   
-    // React says we have to define render!!
-    render() {
-        if (this.state.errorMessage && !this.state.lat)  {
-            return <div>Error: {this.state.errorMessage}</div>;
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  // React says we have to define render!!
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-        return <SeasonDisplay lat={this.state.lat} />
+      return <SeasonDisplay lat={this.state.lat} />;
     }
-    
+
     return <div>Loading!</div>;
-    }
-}   
+  }
+}
 
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root'));
-
-  
+ReactDOM.render(<App />, document.querySelector('#root'));
